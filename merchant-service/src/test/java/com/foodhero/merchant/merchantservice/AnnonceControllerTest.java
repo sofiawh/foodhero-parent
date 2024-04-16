7c459202989268643551a81e552c96291a3f4843
sofiawh <sofia149@yahoo.fr>
2024-04-16 09:41:01 +0100

    commit

--- merchant-service/src/test/java/com/foodhero/merchant/merchantservice/AnnonceControllerTest.java
+++ merchant-service/src/test/java/com/foodhero/merchant/merchantservice/AnnonceControllerTest.java
@@ -35,7 +35,13 @@ public class AnnonceControllerTest {
 
     @Test
     public void testCreateAnnonce() throws Exception {
-        MockMultipartFile file = new MockMultipartFile("file", "test.jpg", MediaType.IMAGE_JPEG_VALUE, Files.readAllBytes(Paths.get(/*System.getProperty("user.home") + "/food/annonces/" + "th.jpeg"*//*"path/to/test.jpg"*/"C:/Users/HP/food/annonces/th.jpeg")));
+
+        MockMultipartFile file = new MockMultipartFile("file", "th.jpeg", MediaType.IMAGE_JPEG_VALUE, getClass().getResourceAsStream("/static/th.jpeg"));
+
+//        byte[] content = Files.readAllBytes(Paths.get(getClass().getResource("/th.jpg").toURI()));
+//        MockMultipartFile file = new MockMultipartFile("file", "th.jpg", MediaType.IMAGE_JPEG_VALUE, content);
+
+//        MockMultipartFile file = new MockMultipartFile("file", "test.jpg", MediaType.IMAGE_JPEG_VALUE, Files.readAllBytes(Paths.get(/*System.getProperty("user.home") + "/food/annonces/" + "th.jpeg"*//*"path/to/test.jpg"*/"C:/Users/HP/food/annonces/th.jpeg")));
         Annonce annonce = new Annonce(); // Créez un objet Annonce à tester
 
         // Définir le comportement attendu lors de l'appel au service
